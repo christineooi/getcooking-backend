271ec79dbe77d34e70796bc76d46ba7eb997a6fd
Christine Ooi <christine.ooi@gmail.com>
2018-07-18 21:37:33 -0400

    Added routes for login, logout and user actions

--- index.js
+++ index.js
@@ -14,33 +14,12 @@ const connectionString = process.env.DATABASE_URL || 'getcookingdb';
 const client = new pg.Client(connectionString);
 client.connect();
 
-// function getCategories(req, res, next) {
-//     console.log(process.env.DATABASE_URL);
-//       console.log("inside getCategories");
-//       client.query('select * from categories')
-//         .then(function (data) {
-//           console.log(data);
-//           res.status(200)
-//             .json({
-//               status: 'success',
-//               data: data.rows,
-//               message: 'Retrieved ALL categories'
-//             });
-//         })
-//         .catch(function (err) {
-//           return next(err);
-//         });
-//     }
-
 const port = process.env.PORT || 3000;
 
 app.post('/login', loginlogout.loginUser);
 app.post('/register', register.registerUser);
 app.post('/user', user.saveRecipe);
 app.post('/user', user.deleteRecipe);
-
-
-// app.get('/categories', getCategories);
 
 app.get('/test', (req, res) => res.send('<h1><marquee>Welcome to Get Cooking!</marquee></h1>'));
 
