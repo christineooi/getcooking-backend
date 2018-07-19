fe5a9358401b643dbf8dcb07aaab2c66ff516b4a
Christine Ooi <christine.ooi@gmail.com>
2018-07-18 21:28:49 -0400

    Added react-redux as dependency

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ const connectionString = process.env.DATABASE_URL || 'getcookingdb';
 const client = new pg.Client(connectionString);
 client.connect();
 
-function getCategories(req, res, next) {
-    console.log(process.env.DATABASE_URL);
-      console.log("inside getCategories");
-      client.query('select * from categories')
-        .then(function (data) {
-          console.log(data);
-          res.status(200)
-            .json({
-              status: 'success',
-              data: data.rows,
-              message: 'Retrieved ALL categories'
-            });
-        })
-        .catch(function (err) {
-          return next(err);
-        });
-    }
+// function getCategories(req, res, next) {
+//     console.log(process.env.DATABASE_URL);
+//       console.log("inside getCategories");
+//       client.query('select * from categories')
+//         .then(function (data) {
+//           console.log(data);
+//           res.status(200)
+//             .json({
+//               status: 'success',
+//               data: data.rows,
+//               message: 'Retrieved ALL categories'
+//             });
+//         })
+//         .catch(function (err) {
+//           return next(err);
+//         });
+//     }
 
 const port = process.env.PORT || 3000;
 
@@ -40,7 +40,7 @@ app.post('/user', user.saveRecipe);
 app.post('/user', user.deleteRecipe);
 
 
-app.get('/categories', getCategories);
+// app.get('/categories', getCategories);
 
 app.get('/test', (req, res) => res.send('<h1><marquee>Welcome to Get Cooking!</marquee></h1>'));
 
